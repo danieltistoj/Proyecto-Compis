38b87099021c38dbb4b5b0bf0da8337abc9216cc
Daniel <josetisrey@gmail.com>
2020-10-17 13:15:53 -0600

    Creacion clase Simbolos

---
+++ src/main/java/Principal/Simbolos.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Principal;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Simbolos {
+ int cantidad;
+    String valor;
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+        
+    }
+
+}
